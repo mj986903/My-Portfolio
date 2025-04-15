4b1e2de1f45885ce1f0c51115eb853e142fe67ee
Mohit Joshi <mj986903@gmail.com>
2025-04-15 10:53:04 +0530

    Initial commit

---
+++ src/data/contact.js
@@ -0,0 +1,32 @@
+import { FaDiscord, FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const ContactData = {
+  phone: "+91 9979645392",
+  email: "mj986903@gmail.com",
+  address: "Rajkot , Gujarat , India - 360001",
+  links: [
+    {
+      url: "https://github.com/mj986903",
+      icon: FaGithub,
+    },
+    {
+      url: "https://www.linkedin.com/in/mj986903",
+      icon: FaLinkedin,
+    },
+    {
+      url: "https://discordapp.com/users/mj986903",
+      icon: FaDiscord,
+    },
+    {
+      url: "https://www.instagram.com/ig_mohit_joshi",
+      icon: FaInstagram,
+    },
+    {
+      url: "https://twitter.com/mj986903",
+      icon: FaXTwitter,
+    },
+  ],
+};
+
+export default ContactData;
---
+++ src/data/profile.js
@@ -0,0 +1,15 @@
+const ProfileData = {
+  img: require("../assets/Images/mohit.png"),
+  logo: require("../assets/Images/name.png"),
+  name: "Mohit Joshi",
+  professions: ["Full Stack Developer", "AI Developer", "Enthusiastic Dev"],
+  info: [
+    "Passionate about crafting dynamic web apps.",
+    "Love blending design and functionality to create seamless tech experiences.",
+    "Always exploring the latest in web and AI innovations.",
+  ],
+  resume:
+    "Mohit.pdf",
+};
+
+export default ProfileData;
