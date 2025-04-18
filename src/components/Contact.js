3af3c768ab4feb12e8ba48436ca9c836c25edb15
Mohit Joshi <122555503+mj986903@users.noreply.github.com>
2025-04-18 11:09:40 +0530

    Change_Contact_Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -51,117 +51,117 @@ const Contact = () => {
         </div>
 
         {/* Contact Wrapper */}
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8  rounded-2xl shadow-2xl p-6 lg:p-10">
-          <div
-            data-aos="fade-left"
-            data-aos-duration="1000"
-            className="bg-darkblue text-white p-6 rounded-xl flex flex-col gap-4"
-          >
-            {/* Header */}
-            <div className="text-center">
-              <h3 className="text-2xl font-semibold text-dark-orange">
-                Get in Touch
-              </h3>
-            </div>
-
-            {/* Social Media Links */}
-            <div className="flex justify-center">
+        <div className="flex flex-col gap-2 md:flex-row w-full mx-auto rounded-xl bg-darkblue p-4 md:gap-7 lg:gap-9 lg:rounded-2xl xl:gap-10">
+          <div className="p-2 w-full text-center lg:p-5 xl:p-7 md:w-1/2 lg:w-4/6">
+            <h1
+              data-aos="zoom-in-down"
+              data-aos-duration="1000"
+              data-aos-once="false"
+              className="hidden md:block text-2xl lg:text-3xl text-dark-orange font-medium mb-3 lg:mb-4"
+            >
+              Get In Touch
+            </h1>
+            <div
+              data-aos="zoom-in-down"
+              data-aos-duration="1000"
+              data-aos-once="false"
+              className="flex gap-5 mb-4 justify-center md:mb-5"
+            >
               <SocialHandles />
             </div>
-
-            {/* Contact Details */}
-            <div className="flex flex-col items-center mt-10">
-              {/* Phone */}
-              <div className="flex flex-col gap-4">
-                {/* Phone */}
-                <div className="flex flex-row items-center gap-4 hover:text-orange-300 transition-colors">
-                  <FaPhoneAlt className="text-2xl" />
-                  <a
-                    href={`tel:${ContactData.phone}`}
-                    className="text-base font-medium"
-                  >
-                    {ContactData.phone}
-                  </a>
-                </div>
-
-                {/* Email */}
-                <div className="flex flex-row items-center gap-4 hover:text-orange-300 transition-colors">
-                  <FaEnvelope className="text-2xl" />
-                  <a
-                    href={`mailto:${ContactData.email}`}
-                    className="text-base font-medium"
-                  >
-                    {ContactData.email}
-                  </a>
-                </div>
-
-                {/* Address */}
-                <div className="flex flex-row items-center gap-4 hover:text-orange-300 transition-colors">
-                  <FaMapMarkerAlt className="text-2xl" />
-                  <a
-                    href="https://www.google.com/maps/place/Rajkot,+Gujarat"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-base font-medium"
-                  >
-                    {ContactData.address}
-                  </a>
-                </div>
-              </div>
+            <div
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-once="false"
+              className="flex gap-3 items-center mb-4 md:gap-2 lg:gap-5"
+            >
+              <FaPhoneAlt className="text-white" />
+              <p className="text-white md:text-lg ">{ContactData.phone}</p>
+            </div>
+            <div
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-once="false"
+              className="flex gap-3 items-center mb-4 md:gap-2 lg:gap-5"
+            >
+              <FaEnvelope className="text-white" />
+              <a
+                href={`mailto:${ContactData.email}`}
+                className="text-white md:text-lg"
+              >
+                {ContactData.email}
+              </a>
+            </div>
+            <div
+              data-aos="fade-right"
+              data-aos-duration="1000"
+              data-aos-once="false"
+              className="flex gap-3 items-center md:gap-2 lg:gap-5"
+            >
+              <FaMapMarkerAlt className="text-white" />
+              <p className="leading-normal text-start text-white md:text-lg">
+                {ContactData.address}
+              </p>
             </div>
           </div>
           <form
-            data-aos="fade-right"
+            data-aos="zoom-in-up"
             data-aos-duration="1000"
+            data-aos-once="false"
             ref={formRef}
             onSubmit={handleSubmit}
-            className="bg-whitesmoke p-8 rounded-xl"
+            className="flex bg-whitesmoke flex-col p-2 rounded-lg md:w-1/2 md:p-4 lg:px-5 lg:py-7 lg:m-4 lg:w-3/5"
           >
-            <h3 className="text-2xl font-semibold text-dark-orange mb-6 text-center">
-              Send Us a Message
-            </h3>
-            <div className="space-y-5">
-              {/* Name Field */}
-              <div>
-                <input
-                  required
-                  name="user_name"
-                  type="text"
-                  placeholder="Your Name"
-                  className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg shadow-sm focus:border-dark-orange focus:ring-2 focus:ring-orange-200 text-gray-900 placeholder-gray-400 transition-all duration-300"
-                />
-              </div>
-
-              {/* Email Field */}
-              <div>
-                <input
-                  required
-                  name="user_email"
-                  type="email"
-                  placeholder="Your Email"
-                  className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg shadow-sm focus:border-dark-orange focus:ring-2 focus:ring-orange-200 text-gray-900 placeholder-gray-400 transition-all duration-300"
-                />
-              </div>
-
-              {/* Message Field */}
-              <div>
-                <textarea
-                  required
-                  name="message"
-                  placeholder="Your Message"
-                  className="w-full px-4 py-3 bg-white border border-gray-300 rounded-lg shadow-sm focus:border-dark-orange focus:ring-2 focus:ring-orange-200 text-gray-900 placeholder-gray-400 h-48 resize-none transition-all duration-300"
-                />
-              </div>
-
-              {/* Submit Button */}
-              <div>
-                <button
-                  type="submit"
-                  className="w-full py-3 px-6 bg-dark-orange text-white font-semibold rounded-lg shadow-md hover:bg-orange-600 hover:scale-105 transition-all duration-300"
-                >
-                  Send Message
-                </button>
-              </div>
+            <div
+              data-aos="zoom-in-up"
+              data-aos-duration="1500"
+              data-aos-once="false"
+              className="p-2 w-full"
+            >
+              <input
+                required
+                placeholder="Name"
+                type="text"
+                name="user_name"
+                className="mb-1 w-full bg-white rounded-md border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-black p-2 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div
+              data-aos="zoom-in-up"
+              data-aos-duration="1500"
+              data-aos-once="false"
+              className="p-2 w-full"
+            >
+              <input
+                required
+                placeholder="Email"
+                type="email"
+                name="user_email"
+                className="mb-1 w-full bg-white rounded-md border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-black p-2 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div
+              data-aos="zoom-in-up"
+              data-aos-duration="1500"
+              data-aos-once="false"
+              className="p-2 w-full"
+            >
+              <textarea
+                required
+                placeholder="Message"
+                name="message"
+                className="mb-1 w-full bg-white rounded-md border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-black p-2 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+            <div
+              data-aos="zoom-in"
+              data-aos-duration="1500"
+              data-aos-once="false"
+              className="p-2 w-full"
+            >
+              <button className=" font-medium mx-auto my-3 text-white bg-dark-orange border-0 py-2 px-12 focus:outline-none hover:scale-110 hover:bg-orange-600 transition duration-500 rounded-xl text-lg">
+                Send
+              </button>
             </div>
           </form>
         </div>
